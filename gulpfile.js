a6a5f613d20c45ccbd0138bc0c07465db9ff85bb
Vladimir Rõkovanov <vladimir.rokovanov@itcollege.ee>
2016-10-08 00:38:59 +0300

    setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 "use strict";
 var gulp =require('gulp');
-var connnect=require('gulp-connect');
+var connect=require('gulp-connect');
 var open = require('gulp-open');
-
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
 var config ={
     port: 9005,
     devBaseUrl:'http://localhost',
     paths:{
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        css:[
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    
+        ],
+        dist : './dist',
+        mainJs: './src/main.js'
     }
 }
 
 gulp.task('connect', function(){
-    connnect.server({
+    connect.server({
         root:['dist'],
         port:config.port,
         base:config.devBaseUrl,
@@ -29,11 +40,35 @@ gulp.task('open',['connect'], function(){
 gulp.task('html',function(){
     gulp.src(config.paths.html)
 .pipe(gulp.dest(config.paths.dist))
-.pipe(connnect.reload());
+.pipe(connect.reload());
 });
+
+gulp.task('js', function(){
+    browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist+'/scripts'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+    gulp.src(config.paths.css)
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest(config.paths.dist+'/css'))
+    .pipe(connect.reload());
+})
+
+gulp.task('lint', function(){
+    return gulp.src(config.paths.js)
+    .pipe(lint({configFile:'eslint.config.json'}))
+    .pipe(lint.format());
+})
 
 gulp.task('watch', function(){
     gulp.watch(config.paths.html,['html']);
+     gulp.watch(config.paths.js,['js', 'lint']);
 })
 
-gulp.task('default',['html','open','watch']);
+gulp.task('default',['html','js','css','lint','open','watch']);
